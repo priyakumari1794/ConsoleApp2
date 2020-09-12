ef88283e855c933654916a356c53c22b96c5f196
Priya Kumari <Priya.Kumari@stats.com>
2020-09-13 00:59:09 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 4;
+            while (x<=10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+         
+        }
+    }
+}
